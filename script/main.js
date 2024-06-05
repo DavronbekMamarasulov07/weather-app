7383184aa62c1a3d8a40ce6bb7a491a5f0c1864a
dev-davronbek <davronbekmamarasulov07@gmail.com>
2024-06-05 14:22:35 +0500

    ass

--- script/main.js
+++ script/main.js
@@ -3,15 +3,35 @@ const $searchInput = document.querySelector("#search-input")
 const $weatherImg = document.querySelector("#weather-img");
 const $weatherIndicator = document.querySelector("#weather-indicator")
 const $weatherLocation = document.querySelector("#weather-location")
+const $heroContentTime = document.querySelector("#hero-content-time")
+const $weatherWater = document.querySelector("#water")
+const $weatherUV = document.querySelector("#uv")
+const $weatherSunset = document.querySelector("#sunset")
+const $weatherSunrise = document.querySelector("#sunrise")
+const $airPressure = document.querySelector("#air-pressure")
+
+
 
 // API KEY
 const API_KEY = "644f6ce0ca9e401ebb891832211707";
 
 
 const renderData = (data) => {
+    console.log(data);
     $weatherImg.src = "https:" + data.current.condition.icon;
     $weatherIndicator.innerText = data.current.temp_c + "°";
     $weatherLocation.innerText = `${data.location.name}, ${data.location.country}`;
+    $heroContentTime.innerText = data.forecast.forecastday[0].astro.sunset;
+    $weatherWater.innerText = data.current.humidity + "%";
+    $weatherUV.innerText = data.current.uv + " out of 10"
+    $weatherSunset.innerText = data.forecast.forecastday[0].astro.sunset;
+    $weatherSunrise.innerText = data.forecast.forecastday[0].astro.sunrise;
+    $airPressure.innerText = data.current.pressure_mb + "Pa"
+
+    
+
+
+
 }
 
 const loadData = (city) =>{
